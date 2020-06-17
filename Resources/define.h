26c351c758c26037f9e1c6875ed0e3982c662dae
cyril <cyril.louisin@outlook.fr>
2020-06-17 22:43:56 +0200

    ressource par defaut

---
+++ Resources/define.h
@@ -0,0 +1,265 @@
+#ifndef __DEFINE_H_BEAST_3D
+#define __DEFINE_H_BEAST_3D
+
+#define FALSE                0 
+#define TRUE                 1
+
+
+#define WUI_NONE              0 
+#define WUI_NOW               1 
+#define WUI_TARGETOBJ         2 
+#define WUI_TARGETOBJ2        3 
+#define WUI_TARGETINGOBJ      4 
+#define WUI_TARGETOBJPTZ      5 
+#define WUI_TARGETPTZ         6 
+#define WUI_TARGETMOVEOBJ     7 
+#define WUI_TARGETMOVEPTZ     8 
+#define WUI_MENU              9 
+#define	WUI_MENU_TOBJ        10 
+#define WUI_TARGETPTZ_IB     11 
+#define WUI_TARGETCURSORPTZ  12 
+
+#define WET_NONE              0 
+#define WET_NOW               1 
+#define WET_DEAD              2 
+#define WET_ATK               3 
+#define WET_ATKOTHER          4 
+#define WET_BODYSTATE         5 
+#define WET_PARRY             6 
+#define WET_ARROW             7 
+#define WET_BULLET            8 
+#define WET_RANGE             9 
+#define WET_MAGIC            10 
+#define WET_HIT              11 
+
+
+#define EXT_NONE            0 
+#define EXT_SELFCHGPARAMET  1 
+#define EXT_OBJCHGPARAMET   2 
+#define	EXT_MAGICSHOT       3 
+#define EXT_MAGICATK        4 
+#define EXT_AMPLIFICATION   5 
+#define EXT_ATTACKER        6 
+#define EXT_MAGIC           7 
+#define EXT_ANOTHER         8 
+#define EXT_ANOTHERWITH     9 
+#define EXT_SUMMON          10 
+#define EXT_AROUNDATK       11 
+#define EXT_OTHER           12 
+#define EXT_TROUPE          13 
+#define EXT_MAGICATKSHOT    14 
+#define EXT_MENTALATK       15 
+#define EXT_MELEEATKSHOT    16 
+#define EXT_MELEEATK        17 
+#define EXT_RANGEATK        18 
+#define EXT_PET             19 
+#define EXT_TROUPEWITH      20 
+#define	EXT_ITEM            21
+
+
+
+#define WES_NONE             0 
+#define WES_WORLD            1 
+#define WES_DEATH            2 
+#define WES_DAMAGE           3 
+#define WES_EXECUTE          4 
+#define WES_DIALOG           5 
+#define WES_EVENT            6 
+#define WES_SCHEDULE_BEGIN   7 
+#define WES_SCHEDULE_END     8 
+
+
+#define SEX_MALE             0
+#define SEX_FEMALE           1
+#define SEX_SEXLESS          2
+
+
+
+#define OT_OBJ          0 
+#define OT_ANI          1 
+#define OT_CTRL         2 
+#define OT_SFX          3 
+#define OT_ITEM         4 
+#define OT_MOVER        5 
+#define OT_REGION       6 
+#define OT_SHIP			7 
+#define OT_PATH			8 
+#define MAX_OBJTYPE     9
+
+
+#define OF_OBJ          0x00000001 
+#define OF_ANI          0x00000002 
+#define OF_CTRL         0x00000004 
+#define OF_SFX          0x00000008 
+#define OF_ITEM         0x00000010
+#define OF_MOVER        0x00000020
+#define OF_REGION       0x00000040
+#define OF_SHIP			0x00000080
+
+
+#define MODELTYPE_NONE               0
+#define MODELTYPE_MESH               1
+#define	MODELTYPE_ANIMATED_MESH		 2
+#define MODELTYPE_BILLBOARD          3 
+#define MODELTYPE_SFX                4 
+#define MODELTYPE_ASE				 5		
+
+
+#define MD_FAR  0 
+#define MD_MID  1 
+#define MD_NEAR 2 
+#define MD_FIX  3 
+
+
+#define ATEX_NONE	0 
+#define ATEX_00		0	
+#define ATEX_USE	1 
+#define ATEX_01		1	
+#define ATEX_02		2	
+#define ATEX_03		3	
+#define ATEX_04		4	
+#define ATEX_05		5	
+#define ATEX_06		6	
+#define ATEX_07		7	
+
+
+#define ITYPE_ITEM      0
+#define ITYPE_CARD      1
+#define ITYPE_CUBE      2 
+#define ITYPE_PET		3
+
+
+#define RA_WORLD         0x00000001
+#define RA_DUNGEON       0x00000002
+#define RA_NEWBIE        0x00000004
+#define RA_BEGIN         0x00000008
+#define RA_SAFETY        0x00000010
+#define RA_SHRINE        0x00000020
+#define RA_FIGHT         0x00000040
+#define RA_INN           0x00000080
+#define RA_SIGHT         0x00000100
+#define RA_DAMAGE        0x00000200
+#define RA_TOWN          0x00000400
+#define RA_DAYLIGHT      0x00000800
+#define RA_PK            0x00001000
+#define RA_OX            0x00002000
+#define RA_DANGER        0x00004000
+#define RA_NO_CHAT       0x00008000
+#define RA_NO_ATTACK     0x00010000
+#define RA_NO_DAMAGE     0x00020000
+#define RA_NO_SKILL      0x00040000
+#define RA_NO_ITEM       0x00080000
+#define RA_NO_TELEPORT   0x00100000
+#define RA_SCHOOL        0x00200000
+#define RA_PENALTY_PK    0x00400000
+#define	RA_COLLECTING	0x00800000
+
+
+#define CUSTOM_LOGO_MAX		27
+
+#define GM_LOGO_CH			21
+#define GM_LOGO_EU			22
+#define GM_LOGO_JP			23
+#define GM_LOGO_PH			24
+#define GM_LOGO_TH			25
+#define GM_LOGO_TW			26
+#define GM_LOGO_US			27
+
+#define CITYN_FLARIS        1
+#define CITYN_SAINTMORNING  2
+
+#define LANDN_FLARIS        1
+#define LANDN_SAINTMORNING  2
+
+
+#define GUIDE_EVENT_MOVE				0
+#define GUIDE_EVENT_KEY_MOVE			1
+#define GUIDE_EVENT_KEY_RUN				2
+#define GUIDE_EVENT_KEY_JUMP			3
+#define GUIDE_EVENT_TRACKING_MOVE		4
+#define GUIDE_EVENT_KEY_ZOOM			5
+#define GUIDE_EVENT_CAMERAMOVE			6
+#define GUIDE_EVENT_KEY_CAMERAMOVE		7
+#define GUIDE_EVENT_INTRO				8
+#define GUIDE_EVENT_END                 9
+#define GUIDE_EVENT_BERSERKERMODE		10
+#define GUIDE_FLIGHT					11
+#define GUIDE_FLIGHT_METHOD				12
+#define APP_SKILL_BEFOREJOB				13
+#define APP_SKILL_AFTERJOB				14
+#define APP_DEATH_FIELD					15
+#define GUIDE_ESSENSE					16
+#define GUIDE_CHANGEJOB					17
+#define GUIDE_APP_GUILD					18
+#define GUIDE_EVENT_MOUSE_MOVE					19
+#define GUIDE_EVENT_KEY_WALK					20
+#define GUIDE_EVENT_MOUSE_CAMERAMOVE			21
+#define GUIDE_EVENT_WORLD_MAP					22
+#define GUIDE_EVENT_WORLD_NAVI					23
+#define GUIDE_EVENT_NORMAL_CHAT					24
+#define GUIDE_EVENT_SHOUT_CHAT					25
+#define GUIDE_EVENT_TROUPE_CHAT					26
+#define GUIDE_EVENT_WHISPER_CHAT				27
+#define GUIDE_EVENT_GUILD_CHAT					28
+#define GUIDE_EVENT_SYSTEM_CHAT					29
+#define GUIDE_EVENT_FILTER_CHAT					30
+#define GUIDE_EVENT_STATUS_WIN					31
+#define GUIDE_EVENT_CHARACTER_WIN				32
+#define GUIDE_EVENT_INVENTORY_WIN				33
+#define GUIDE_EVENT_QUEST_WIN					34
+#define GUIDE_EVENT_QUICKSLOT					35
+#define GUIDE_EVENT_TROUPE						36
+#define GUIDE_EVENT_TROUPE_WIN					37
+#define GUIDE_EVENT_CHARACTER_MENU				38
+#define GUIDE_EVENT_TRADE						39
+#define GUIDE_EVENT_STORE						40
+#define GUIDE_EVENT_BANK						41
+#define GUIDE_EVENT_MESSENGER					42
+#define GUIDE_EVENT_MOTION						43
+#define GUIDE_EVENT_CHEER						44
+
+
+#define		MAX_SUMMON		8		
+
+
+#define UA_ITEM          0x00000001
+#define UA_LEVEL         0x00000002
+#define UA_QUEST         0x00000004
+#define UA_CLASS         0x00000008
+#define UA_GENDER        0x00000010
+#define UA_TELEPORT      0x00000020
+#define UA_QUEST_END     0x00000040
+#define UA_PLAYER_ID     0x00000080
+
+
+#define TOT_RANDOM			0    
+#define TOT_NOENDU		1    
+
+#define	PK_TIGER	0
+#define	PK_LION		1
+#define	PK_RABBIT	2
+#define	PK_FOX	3
+#define	PK_DRAGON	4
+#define	PK_GRIFFIN	5
+#define	PK_UNICORN	6
+#define	PK_MAX	7
+
+
+#define	TI_GENERIC	0
+#define	TI_PET	1
+
+#define	PERIN_VALUE		100000000L
+
+
+#define POINTTYPE_CAMPUS	1
+
+
+#define SEASON_NONE			0
+#define SEASON_SPRING		1
+#define SEASON_SUMMER		2
+#define SEASON_FALL			3
+#define SEASON_WINTER		4
+#define SEASON_MAX			5
+
+
+#endif
---
+++ Resources/defineItemkind.h
@@ -0,0 +1,242 @@
+#ifndef __DEFINE_ITEMKIND
+#define __DEFINE_ITEMKIND
+
+
+
+
+
+
+#define IK1_GOLD                   0
+#define IK1_WEAPON                 1
+#define IK1_ARMOR                  2
+#define IK1_GENERAL                3
+#define IK1_RIDE                   4
+#define IK1_SYSTEM                 5
+#define IK1_CHARGED				   6
+#define IK1_HOUSING					7	
+
+
+
+
+#define IK2_GOLD                   0
+#define IK2_WEAPON_HAND            1
+#define IK2_WEAPON_DIRECT          2
+#define IK2_WEAPON_MAGIC           3
+#define IK2_ARMOR                  7
+#define IK2_ARMORETC               8
+#define IK2_CLOTH                  9
+#define IK2_CLOTHETC              10
+#define IK2_REFRESHER             11
+#define IK2_POTION                12
+#define IK2_JEWELRY               13
+#define IK2_FOOD                  14
+#define IK2_MAGIC                 15
+#define IK2_GEM                   16
+#define IK2_MATERIAL              17
+#define IK2_TOOLS                 18
+#define IK2_SYSTEM                19
+#define IK2_RIDING                20
+#define IK2_MOB                   21
+#define IK2_BLINKWING             22
+#define IK2_AIRFUEL               23
+#define IK2_CHARM                 24
+#define IK2_BULLET                25
+#define IK2_TEXT                  26
+#define IK2_GMTEXT				  27
+#define IK2_GENERAL				  28
+#define	IK2_BUFF					29	
+#define	IK2_WARP				30
+#define IK2_SKILL				31
+#define IK2_CLOTHWIG			32	
+#define	IK2_BUFF2		33	
+#define IK2_FURNITURE		34	
+#define IK2_PAPERING		35	
+#define	IK2_TOCASH		36	
+#define	IK2_BUFF_TOGIFT		37	
+#define	IK2_GUILDHOUSE_FURNITURE		38
+#define	IK2_GUILDHOUSE_NPC		39
+#define IK2_GUILDHOUSE_PAPERING 40
+#define	IK2_GUILDHOUES_COMEBACK	41
+
+
+
+
+
+#define IK3_GOLD                   0
+
+#define IK3_HAND                   1
+#define IK3_SWD                    2
+#define IK3_AXE                    3
+#define IK3_CHEERSTICK             4
+#define IK3_KNUCKLEHAMMER          5
+#define IK3_WAND                   6
+#define IK3_STAFF                  7
+#define IK3_THSWD                  8
+#define IK3_THAXE                  9
+#define IK3_VIRTUAL               10
+#define IK3_YOYO                  11
+#define IK3_BOW                   12
+#define IK3_YOBO				  13
+
+
+#define IK3_SHIELD                16
+#define IK3_HELMET                17
+#define IK3_SUIT                  18
+#define IK3_GAUNTLET              19
+#define IK3_BOOTS                 20
+
+
+#define IK3_HAT                   21
+#define IK3_MASK                  22
+#define IK3_SHOES                 23
+#define IK3_CLOAK                 24
+#define IK3_CLOTH                 57
+#define IK3_GLOVE                 58
+
+
+#define IK3_REFRESHER             25
+#define IK3_POTION                26
+#define IK3_EARRING               27
+#define IK3_NECKLACE              28
+#define IK3_RING                  29
+#define IK3_INSTANT               30
+#define IK3_COOKING               31
+#define IK3_ICECEARM              32
+#define IK3_PILL                  59
+#define IK3_MAGICTRICK            33
+#define IK3_GEM                   34
+#define IK3_DRINK                 35
+#define IK3_COLLECTER		      36
+#define IK3_ELECARD		          37
+#define IK3_DICE		          38
+#define IK3_SUPSTONE		      39
+ 
+
+#define IK3_BOARD                 40
+#define IK3_STICK                 41
+#define IK3_EVENTMAIN             42
+#define IK3_QUEST                 43
+#define IK3_MAP                   44
+#define IK3_BLINKWING             45
+#define IK3_EVENTSUB              46
+#define IK3_TOWNBLINKWING         47
+
+
+#define IK3_ACCEL		          48
+#define IK3_DELETE		          49
+
+
+#define IK3_SCROLL		        50
+#define IK3_ENCHANTWEAPON		51
+#define IK3_CFLIGHT		        52
+#define IK3_BFLIGHT		        53
+#define IK3_MAGICBOTH			54
+#define IK3_BCHARM		        55
+#define IK3_RCHARM		        56 
+#define IK3_ARROW		        60
+
+
+#define IK3_PIERDICE			61
+#define IK3_SOCKETCARD			62
+#define IK3_SOCKETCARD2			63
+
+
+#define IK3_TEXT_BOOK           	70
+#define IK3_TEXT_SCROLL	        	71
+#define IK3_TEXT_LETTER         	72
+
+
+#define	IK3_TEXT_UNDYING		80
+#define	IK3_TEXT_DISGUISE		81
+#define IK3_TEXT_INVISIBLE		82
+#define IK3_TEXT_GM			83
+
+
+#define	IK3_BINDS			84
+
+
+#define IK3_CREATE_MONSTER		85
+
+#define IK3_POTION_BUFF_STR		90		
+#define IK3_POTION_BUFF_DEX		91		
+#define IK3_POTION_BUFF_INT		92		
+#define IK3_POTION_BUFF_STA		93		
+#define IK3_POTION_BUFF_DEFENSE	94		
+
+#define IK3_ANGEL_BUFF			95		
+
+
+#define IK3_PET				100		
+#define IK3_RANDOM_SCROLL		101		
+
+#define IK3_ULTIMATE			102		
+
+#define	IK3_LINK	104
+
+#define IK3_GENERAL			118		
+
+#define IK3_ENCHANT			119
+
+#define IK3_EGG		120		
+#define	IK3_FEED	121
+
+#define	IK3_TICKET	122		
+#define	IK3_POCKET	123		
+
+#define IK3_BED			124		
+#define IK3_SOFA		125		
+#define IK3_WARDROBE	126		
+#define IK3_CLOSET		127		
+#define IK3_TABLE		128		
+#define IK3_CABINET		129		
+#define IK3_PROPS		130		
+#define IK3_WALLPAPER	131		
+#define IK3_CARPET		132		
+#define	IK3_COUPLE_BUFF	133		
+#define	IK3_FUNNYCOIN	134		
+#define	IK3_FLOWER	135		
+#define IK3_BALLOON	136		
+#define IK3_WING	137		
+
+#define IK3_VIS		138		
+#define	IK3_TS_BUFF	139
+
+#define IK3_TELEPORTER		140
+#define IK3_REST			141	
+#define	IK3_DESK			142	
+#define	IK3_CHAIR			143	
+#define	IK3_CASE			144	
+#define	IK3_BATH			145	
+#define	IK3_DRAWER			146	
+
+#define	IK3_CRYSTAL			147
+
+#define IK3_KEY				148
+#if defined(__BUFF_SERVER_PLAYER)
+#define IK3_BUFF_SERVER     149
+#endif //__BUFF_SERVER_PLAYER
+
+#define MAX_ITEM_KIND3      150	
+#define MAX_UNIQUE_SIZE	    400
+
+
+#define CK1_CHEST				0   
+#define CK1_DOOR				1   
+#define CK1_TRIGGER				2   
+#define CK1_HOUSING				3   
+#define	CK1_GUILD_HOUSE				4   
+
+#define CK2_FADE				1   
+#define CK2_KEEP				2   
+
+#define CK3_FULL				1   
+#define CK3_HALF				2   
+
+#define WEAPON_GENERAL			0	
+#define WEAPON_UNIQUE			1	
+#define WEAPON_ULTIMATE			2	
+
+#define ARMOR_SET				3	
+
+#define PET_VIS					1	
+#endif
